da30c58798d4262bcee79ebbd96beab02770d8fa
Rahul Bhargava <rahulbot@gmail.com>
2022-03-21 16:09:19 -0400

    save compressed files #2

--- scripts/gen_daily_story_rss.py
+++ scripts/gen_daily_story_rss.py
@@ -5,6 +5,8 @@ import os.path
 from feedgen.feed import FeedGenerator
 from sqlalchemy import text
 import mcmetadata.domains as domains
+import gzip
+import shutil
 
 import fetcher.feedgen.ext.mediacloud
 from fetcher import base_dir, VERSION, engine
@@ -19,46 +21,56 @@ if __name__ == '__main__':
     logger.info("Writing daily RSS files since {}".format(today))
 
     # generate a file for each of the last 30 days
-    for d in range(1, 30):
+    for d in range(1, 60):
         day = today - datetime.timedelta(d)
         logger.info(" Working on {} (day {})".format(day, d))
         # only do this day if it doesn't exist already
         filename = "mc-{}.rss".format(day.strftime("%Y-%m-%d"))
         filepath = os.path.join(TARGET_DIR, filename)
-        if os.path.exists(filepath):
-            logger.warning("  Skipping - file already exists at {}".format(filepath))
-            continue
-        # start a feed
-        fg = FeedGenerator()
-        #fg.load_extension('mediacloud')
-        fg.register_extension('mediacloud',
-                              fetcher.feedgen.ext.mediacloud.MediacloudExtension,
-                              fetcher.feedgen.ext.mediacloud.MediacloudEntryExtension)
-        fg.title("Media Cloud URL Snapshot for {}".format(day))
-        fg.description("Auto generated feed of all stories discovered on the specified day - {}".format(VERSION))
-        fg.link(href="https://mediacloud.org/")
-        # metadata
-        # grab the stories fetched on that day
-        story_count = 0
-        query = "select id, url, guid, published_at from stories where fetched_at::date = '{}'::date".format(
-            day.strftime("%Y-%m-%d"))
-        with engine.begin() as connection:  # will auto-close
-            result = connection.execute(text(query))
-            for row in result:
-                story = dict(row)
-                fe = fg.add_entry()
-                fe.id(story['guid'])
-                fe.title(story['title'] if 'title' in story else None)
-                fe.link(href=story['url'])
-                fe.pubDate(story['published_at'])
-                fe.content("")
-                try:
-                    fe.mediacloud.canonical_domain = domains.from_url(story['url'])
-                except Exception as e:
-                    logger.error("Couldn't get canonical domain {}".format(e))
-                story_count += 1
-        fg.rss_file(filepath)
-        logger.info("   Found {} stories".format(story_count))
-        logger.info("   Wrote out to {}".format(filepath))
+        compressed_filepath = '{}.gz'.format(filepath)
+        if not os.path.exists(compressed_filepath):
+            # start a feed
+            fg = FeedGenerator()
+            #fg.load_extension('mediacloud')
+            fg.register_extension('mediacloud',
+                                  fetcher.feedgen.ext.mediacloud.MediacloudExtension,
+                                  fetcher.feedgen.ext.mediacloud.MediacloudEntryExtension)
+            fg.title("Media Cloud URL Snapshot for {}".format(day))
+            fg.description("Auto generated feed of all stories discovered on the specified day - {}".format(VERSION))
+            fg.link(href="https://mediacloud.org/")
+            # metadata
+            # grab the stories fetched on that day
+            story_count = 0
+            query = "select id, url, guid, published_at from stories where fetched_at::date = '{}'::date".format(
+                day.strftime("%Y-%m-%d"))
+            with engine.begin() as connection:  # will auto-close
+                result = connection.execute(text(query))
+                for row in result:
+                    story = dict(row)
+                    fe = fg.add_entry()
+                    fe.id(story['guid'])
+                    fe.title(story['title'] if 'title' in story else None)
+                    fe.link(href=story['url'])
+                    fe.pubDate(story['published_at'])
+                    fe.content("")
+                    try:
+                        fe.mediacloud.canonical_domain = domains.from_url(story['url'])
+                    except Exception as e:
+                        logger.error("Couldn't get canonical domain {}".format(e))
+                    story_count += 1
+            fg.rss_file(filepath)
+            logger.info("   Found {} stories".format(story_count))
+            logger.info("   Wrote out to {}".format(filepath))
+            # compress for storage and transmission speed
+            # https://stackoverflow.com/questions/38018071/how-to-compress-a-large-file-in-python
+            if not os.path.exists(compressed_filepath):
+                with open(filepath, 'rb') as f_in:
+                    with open(compressed_filepath, 'wb') as f_out1:
+                        with gzip.GzipFile(filepath, 'wb', fileobj=f_out1) as f_out2:
+                            shutil.copyfileobj(f_in, f_out2)
 
+        else:
+            logger.warning("  Skipping - file already exists at {}".format(compressed_filepath))
+        # and delete the uncompressed to save space
+        os.remove(filepath)
     logger.info("Done")
