f786f327c9847bd9ce4ea28f4c8aaae67cff6a50
Rahul Bhargava <rahulbot@gmail.com>
2022-05-16 16:14:14 -0400

    add new data import

---
+++ fetcher/database/versions/a83638d0280b_update_feeds.py
@@ -0,0 +1,54 @@
+"""update-feeds
+
+Revision ID: a83638d0280b
+Revises: 8f9a42e8903c
+Create Date: 2022-05-16 15:43:26.041869
+
+"""
+import csv
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'a83638d0280b'
+down_revision = '8f9a42e8903c'
+branch_labels = None
+depends_on = None
+
+
+# Data generated with this command on the production database:
+#   psql -c "\copy (select * from feeds f where f.active=true and f.type='syndicated' and f.last_new_story_time > now() - '180 days'::interval) TO ‘/tmp/feeds-2022-05-16.csv' CSV HEADER;"
+def upgrade():
+    # 133,830 -> 144,269
+    # add a column to track import batches
+    op.add_column('feeds', sa.Column('import_round', sa.Integer()))
+    op.execute("UPDATE feeds SET import_round = 1")
+    # grab the ids from the last import file
+    first_csv_file_path = 'data/feeds-2022-02-16.csv'
+    with open(first_csv_file_path) as csvfile:
+        reader = csv.DictReader(csvfile)
+        prepopulated_ids = [r['feeds_id'] for r in reader]
+    # insert (https://alembic.sqlalchemy.org/en/latest/ops.html?highlight=bulk_insert#alembic.operations.Operations.bulk_insert)
+    feeds_table = sa.sql.table('feeds',
+                               sa.sql.column('mc_feeds_id', sa.BigInteger),
+                               sa.sql.column('mc_media_id', sa.String),
+                               sa.sql.column('name', sa.String),
+                               sa.sql.column('url', sa.String),
+                               sa.sql.column('type', sa.String),
+                               sa.sql.column('active', sa.Boolean),
+                               sa.sql.column('import_round', sa.Integer)
+                               )
+    # now load in new file, and import all except already existing ides
+    new_csv_file_path = 'data/feeds-2022-05-16.csv'
+    with open(new_csv_file_path) as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = [r for r in reader]
+        new_data = [d for d in data if d['feeds_id'] not in prepopulated_ids]
+        for r in new_data:
+            r['active'] = True if r['active'] == 't' else False
+            r['import_round'] = 2
+        op.bulk_insert(feeds_table, new_data)
+
+
+def downgrade():
+    op.execute("DELETE from feeds WHERE import_round = 2")
--- fetcher/database/versions/a91f46836029_prepopulate_feeds.py
+++ fetcher/database/versions/a91f46836029_prepopulate_feeds.py
@@ -16,6 +16,8 @@ branch_labels = None
 depends_on = None
 
 
+# Data generated with this command on the production database:
+#   psql -c "\copy (select * from feeds f where f.active=true and f.type='syndicated' and f.last_new_story_time > now() - '180 days'::interval) TO ‘/tmp/feeds-2022-02-16.csv' CSV HEADER;"
 def upgrade():
     csv_file_path = 'data/feeds-2022-02-16.csv'
     # insert (https://alembic.sqlalchemy.org/en/latest/ops.html?highlight=bulk_insert#alembic.operations.Operations.bulk_insert)
