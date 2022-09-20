364e53305d15977de4b001c3f0fd3ef670abccd7
Phil Budne <pbudne@angwin.cs.umass.edu>
2022-09-20 16:49:18 -0400

    add comment, git info to install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# "predeploy" script (see app.json) invoked by dokku
+echo git branch $(git branch --show-current)
+echo git hash $(git rev-parse --verify HEAD)
+echo git tags $(git tag --points-at HEAD)
 alembic upgrade head
