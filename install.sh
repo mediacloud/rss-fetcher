cb51186138e2d4aec8db8c87a4f0885d9fbfe752
Phil Budne <pbudne@angwin.cs.umass.edu>
2022-09-22 18:10:26 -0400

    install.sh: replace git output with cwd

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # "predeploy" script (see app.json) invoked by dokku
-echo git branch $(git branch --show-current)
-echo git hash $(git rev-parse --verify HEAD)
-echo git tags $(git tag --points-at HEAD)
+echo cwd `pwd`
 alembic upgrade head
