55a1204f54ee62420fab4c9929c06df3743b6e7a
Phil Budne <phil@ultimate.com>
2022-09-30 15:14:15 -0400

    Procfile: use ./run-XXX.sh; run-rss-workers.sh: obey MC_WORKER_{LOG_LEVEL,CONCURRENCY} run-server.sh: obey PORT

--- run-rss-workers.sh
+++ run-rss-workers.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
+
+# now invoked from Procfile
+
+if [ "x$MC_WORKER_LOG_LEVEL" = x ]; then
+    if [ "x$GIT_REV" = x ]; then
+	# invoked outside Dokku; use old run-rss-workers.sh default
+	MC_WORKER_LOG_LEVEL=debug
+    else
+	# invoked inside Dokku; use old Procfile default
+	MC_WORKER_LOG_LEVEL=info
+    fi
+fi
+
+if [ "x$MC_WORKER_CONCURRENCY" = x ]; then
+    # must be below DB_POOL_SIZE
+    # (XXX check? use MC_WORKER_CONCURRENCY to determine pool size??)
+    MC_WORKER_CONCURRENCY=16
+fi
+
+echo ==== clear_queue
 python -m scripts.clear_queue
-python -m fetcher.main -A fetcher worker -l debug --concurrency=16
+echo ==== worker
+python -m scripts.worker -A fetcher worker \
+       -l $MC_WORKER_LOG_LEVEL \
+       --concurrency=$MC_WORKER_CONCURRENCY
--- run-server.sh
+++ run-server.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-uvicorn server:app --timeout-keep-alive 500 --host 0.0.0.0 --port 8000
+# use PORT if supplied (by Dokku)
+uvicorn server:app --timeout-keep-alive 500 --host 0.0.0.0 --port ${PORT:-8000}
