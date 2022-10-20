675a4d7cfb9831f9ecf6b3e8f55960792559e789
Phil Budne <phil@ultimate.com>
2022-10-20 18:58:02 -0400

    autopep8.sh: skip venv directory

--- autopep8.sh
+++ autopep8.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-find . -name versions -prune -o -name \*.py -print | xargs autopep8 -a -i
+find . -name versions -prune -o \
+     -name venv -prune -o \
+     -name \*.py -print | xargs autopep8 -a -i
