184a952dae65cce306bb5939a1cc0e338f8c2535
Phil Budne <phil@ultimate.com>
2022-10-21 18:09:15 -0400

    fetcher/database/functions.py: add typing

--- fetcher/database/functions.py
+++ fetcher/database/functions.py
@@ -2,23 +2,30 @@
 Postgresql functions not (yet?) supplied by SQLAlchemy
 """
 
+from typing import Any
+
 from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.sql.compiler import TypeCompiler
 import sqlalchemy.sql.functions as functions
 from sqlalchemy.sql import expression
 from sqlalchemy.types import Numeric
 
 # Phil October 2022
 if hasattr(functions, 'greatest'):
-    greatest = functions.greatest
+    greatest = functions.greatest  # type: ignore[attr-defined]
 else:
     # https://docs.sqlalchemy.org/en/14/core/compiler.html#greatest-function
     # (NOTE: has special cases for non-PG dbs)
 
-    class greatest(expression.FunctionElement):
+    class _greatest(expression.FunctionElement):
         type = Numeric()
         name = 'greatest'
         inherit_cache = True
 
-    @compiles(greatest)
-    def default_greatest(element, compiler, **kw):
+    @compiles(_greatest)
+    def default_greatest(element: _greatest,
+                         compiler: TypeCompiler,
+                         **kw: Any) -> str:
         return compiler.visit_function(element)
+
+    greatest = _greatest
