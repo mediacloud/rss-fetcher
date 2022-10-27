26205215b2b51c786da2acf7f3ea88e131be332c
Phil Budne <phil@ultimate.com>
2022-10-27 14:09:57 -0400

    indices: add feeds_system_enabled, feeds_next_fetch_attempt, drop feeds_last_fetch_attempt

---
+++ fetcher/database/versions/20221027_1406_indices.py
@@ -0,0 +1,26 @@
+"""indices: add feeds_system_enabled, feeds_next_fetch_attempt, drop feeds_last_fetch_attempt
+
+Revision ID: 5a54dacd62e3
+Revises: 9d6e6264b580
+Create Date: 2022-10-27 14:06:27.295729
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5a54dacd62e3'
+down_revision = '9d6e6264b580'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_index('feeds_last_fetch_attempt', 'feeds')
+    op.create_index('feeds_system_enabled', 'feeds', ['system_enabled'])
+    op.create_index('feeds_next_fetch_attempt', 'feeds', ['last_fetch_attempt'])
+
+
+def downgrade():
+    op.create_index('feeds_last_fetch_attempt', 'feeds', ['last_fetch_attempt'])
