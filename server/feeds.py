0c1b4b08225ebc9a5b6802dd800e5f7af8a7acc5
Phil Budne <phil@ultimate.com>
2022-10-28 14:16:56 -0400

    server/feeds.py: fix get_feed_history function name; add /api/feeds/ID

--- server/feeds.py
+++ server/feeds.py
@@ -1,11 +1,10 @@
 import logging
-from typing import List, Dict
+from typing import Dict, List, Optional
 from fastapi import APIRouter
-from sqlalchemy import select
 
 from fetcher.database import Session
 from server.util import api_method
-from fetcher.database.models import FetchEvent
+from fetcher.database.models import Feed, FetchEvent
 
 logger = logging.getLogger(__name__)
 
@@ -17,9 +16,22 @@ router = APIRouter(
 
 @router.get("/{feed_id}/history")
 @api_method
-def stories_fetched_counts(feed_id: int) -> List[Dict]:
+def get_feed_history(feed_id: int) -> List[Dict]:
     with Session() as session:
+        # XXX limit rows returned (by date or count)??
         fetch_events = session.query(FetchEvent)\
                               .filter(FetchEvent.feed_id == feed_id)\
+                              .order_by(Feed.id.desc())\
                               .all()
         return [fe.as_dict() for fe in fetch_events]
+
+
+@router.get("/{feed_id}")
+@api_method
+def get_feed(feed_id: int) -> Optional[Dict]:
+    with Session() as session:
+        feed: Optional[Feed] = session.get(Feed, feed_id)
+        if feed:
+            return feed.as_dict()
+        else:
+            return None
