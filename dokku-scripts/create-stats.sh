b63f5d5e90a7762b9125249dbe2879e67dfd5372
Phil Budne <pbudne@angwin.cs.umass.edu>
2022-11-09 01:08:13 +0000

    update dokku-scripts for stats service creation on public servers.

--- dokku-scripts/create-stats.sh
+++ dokku-scripts/create-stats.sh
@@ -17,31 +17,37 @@ fi
 
 HOST=$(hostname -s)
 
-if ! dokku graphite:exists $GRAPHITE_STATS_SVC; then
+if dokku graphite:exists $GRAPHITE_STATS_SVC >/dev/null 2>&1; then
+    echo found  graphite service $GRAPHITE_STATS_SVC
+else
+    echo creating graphite service $GRAPHITE_STATS_SVC
     dokku graphite:create $GRAPHITE_STATS_SVC
+fi
 
-    if [ "x$HOST" = "x$BASTION" ]; then
-	# establish proxy app for local service letsencrypt cert
-
-	# not yet tested whether this is kosher:
-	STATS_PROXY_APP=$GRAPHITE_STATS_SVC
-
-	# script not yet tested!!!
-	$SCRIPT_DIR/stats-service-proxy.sh $GRAPHITE_STATS_SVC $STATS_PROXY_APP
+if public_server; then
+    # give the encrypted service an obvious/pleasant name:
+    STATS_PROXY_APP=stats
+    if dokku apps:exists $STATS_PROXY_APP >/dev/null 1>&2; then
+	echo found proxy app $STATS_PROXY_APP
     else
-	# use unencrypted service on non-public server
-	# (cannot get letsencrypt cert on server that isn't Internet visible)
-
-	# dokku app name to create on BASTION host: ie; stats.OURHOST
-	BASTION_SERVICE=$GRAPHITE_STATS_SVC.$HOST
-
-	# expose dokku-graphite http service on port 80
-	# and recognize a domain name like stats.OURHOST.tarbell.mediacloud.org
-	dokku graphite:nginx-expose $GRAPHITE_STATS_SVC $BASTION_SERVICE.$BASTION.$PUBLIC_DOMAIN
-
-	# give instructions on how set up encrypted proxy on BASTION host:
-	echo "NOTE!!! run '$SCRIPT_DIR/http-proxy.sh $BASTION_SERVICE $HOST 80' on $BASTION"
+	# establish proxy app for local service letsencrypt cert
+	$SCRIPT_DIR/stats-service-proxy.sh $GRAPHITE_STATS_SVC $STATS_PROXY_APP
     fi
+    FULLNAME=$STATS_PROXY_APP.$(hostname -s).$PUBLIC_DOMAIN
+else
+    # use unencrypted service on non-public server
+    # (cannot get letsencrypt cert on server that isn't Internet visible)
+
+    # dokku app name to create on BASTION host: ie; stats.OURHOST
+    BASTION_SERVICE=$GRAPHITE_STATS_SVC.$HOST
+
+    # expose dokku-graphite http service on port 80
+    # and recognize a domain name like stats.OURHOST.tarbell.mediacloud.org
+    FULLNAME=$BASTION_SERVICE.$BASTION.$PUBLIC_DOMAIN
+    dokku graphite:nginx-expose $GRAPHITE_STATS_SVC $FULLNAME
+
+    # give instructions on how set up encrypted proxy on BASTION host:
+    echo "NOTE!!! run '$SCRIPT_DIR/http-proxy.sh $BASTION_SERVICE $HOST 80' on $BASTION"
 fi
 if [ "x$LINK_TO_APP" != x ]; then
     if ! dokku graphite:linked $GRAPHITE_STATS_SVC $LINK_TO_APP; then
@@ -49,3 +55,5 @@ if [ "x$LINK_TO_APP" != x ]; then
 	dokku graphite:link $GRAPHITE_STATS_SVC $LINK_TO_APP
     fi
 fi
+
+echo "**** connect to https://$FULLNAME user admin password admin to reset admin password ****"
--- dokku-scripts/stats-service-proxy.sh
+++ dokku-scripts/stats-service-proxy.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 
-# create proxy app for letsencrypt certificate for dokku-graphite service
-# (letsencypt plugin only supports certs for apps)
 # Phil Budne, October 2022
 
-# NOT YET TESTED!!!
+# NOTE! this script normally invoked from dokku-scripts/create-stats.sh
 
-# from https://github.com/dokku/dokku-graphite/issues/18
-# "In this example, a graphite service named $SERVICE will be exposed under the app $PROXY_APP"
-# (unknown as yet whether SERVICE == PROXY_APP will work)
+# create proxy app for letsencrypt https certificate for a LOCAL
+# dokku-graphite service (letsencypt plugin only supports certs for apps).
 
+# If you want a proxy for a stats server on a server which is on a
+# server that isn't Internet visible, see dokku-scripts/http-proxy.sh
+
+# from https://github.com/dokku/dokku-graphite/issues/18#issuecomment-917603293
+# suggestion by Jose Diaz-Gonzalez (Señor Dokku?)
+
+# name of stats service (should be obscure, since it will be visible at port 80):
 SERVICE=$1
+
+# name of proxy app (publicly visible name):
 PROXY_APP=$2
 
+if [ "x$SERVICE" = x -o "x$PROXY_APP" = x -o "x$SERVICE" = "x$PROXY_APP" ]; then
+    echo Usage: $0 GRAPHITE_SERVICE_NAME PROXY_APP_NAME 1>&2
+    exit 1
+fi
 HOST=$(hostname -s)
 
 SCRIPT_DIR=$(dirname $0)
@@ -25,21 +35,28 @@ fi
 
 DOMAIN=$BASTION.$PUBLIC_DOMAIN
 
-# test for angwin cluster:
 if ! public_server; then
     echo must be run on Internet visible server 1>&2
     exit 1
 fi
 
-if dokku apps:list | grep -F -x $PROXY_APP >/dev/null; then
+if ! dokku graphite:exists $SERVICE >/dev/null 2>&1; then
+    echo dokku-graphite service $SERVICE not found
+    exit 1
+fi
+
+if dokku apps:exists $PROXY_APP >/dev/null 2>&1; then
     echo Dokku app $PROXY_APP exists
     exit 1
 fi
 
 echo creating service-proxy app $PROXY_APP for graphite https access
+set -x
 dokku apps:create $PROXY_APP
 dokku config:set $PROXY_APP SERVICE_NAME=$SERVICE SERVICE_TYPE=graphite SERVICE_PORT=80
 dokku graphite:link $SERVICE $PROXY_APP
 dokku git:from-image $PROXY_APP dokku/service-proxy:latest
-dokku domains:set $PROXY_APP $PROXY_APP $PROXY_APP.$DOMAIN
+dokku domains:set $PROXY_APP $PROXY_APP.$DOMAIN
+# not in global config on tarbell:
+dokku config:set $PROXY_APP --no-restart DOKKU_LETSENCRYPT_EMAIL=$DOKKU_LETSENCRYPT_EMAIL
 dokku letsencrypt:enable $PROXY_APP
