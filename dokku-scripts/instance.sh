02e2c92c6ade1b0be6dbaaaa74d2a7389c869e57
Phil Budne <pbudne@angwin.cs.umass.edu>
2022-11-09 00:03:03 -0500

    dokku-scripts/instance.sh: remove DATABASE_SVC debug

--- dokku-scripts/instance.sh
+++ dokku-scripts/instance.sh
@@ -69,8 +69,6 @@ DATABASE_SVC=$APP
 if [ "x$NAME" = xprod ] && dokku postgres:exists rss-fetcher-db >/dev/null 2>&1; then
     DATABASE_SVC=rss-fetcher-db
 fi
-echo DATABASE_SVC $DATABASE_SVC
-exit
 
 # storage for generated RSS files, db CSV files, logs
 STORAGE=${APP}-storage
