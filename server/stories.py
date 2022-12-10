c3a0422d70818cbbc560cbb4b216a7f13ced734e
Phil Budne <phil@ultimate.com>
2022-12-09 19:04:17 -0500

    /api/stories/by-source endpoint working

--- server/stories.py
+++ server/stories.py
@@ -42,8 +42,16 @@ def stories_published_counts() -> TimeSeriesData:
 @api_method
 def stories_by_source() -> Dict[str, object]:
     with Session() as session:
-        counts = session.query(Story.sources_id, func.count(Story.id))\
-                        .group_by(Story.sources_id).all()
-        dates = session.query(func.max(Story.fetched_at),
-                              func.min(Story.fetched_at)).one()
-    return {'dates': dates, 'sources': list(counts)}
+        counts = session.query(Story.sources_id.label('sources_id'),
+                               func.count(Story.id).label('count'))\
+            .group_by(Story.sources_id)
+        dates = session.query(func.max(Story.fetched_at).label('max'),
+                              func.min(Story.fetched_at).label('min')).one()
+        min = dates['min'].timestamp()
+        max = dates['max'].timestamp()
+        SECONDS_PER_DAY = 26 * 60 * 60
+        ret = {
+            'days': (max - min) / SECONDS_PER_DAY,
+            'sources': [dict(count) for count in counts]
+        }
+    return ret
