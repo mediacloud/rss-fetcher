434a31fa74be11cb4c6a60d226ca17ff9b5a94a1
Phil Budne <phil@ultimate.com>
2022-12-19 19:58:09 -0500

    scripts/server.py: add --workers command line argument (default to 4)

--- scripts/server.py
+++ scripts/server.py
@@ -20,12 +20,18 @@ if __name__ == '__main__':
     # honor environment variable passed by Dokku:
     def_port = int(os.getenv('PORT', '8000'))
     def_host = '0.0.0.0'
+    def_workers = 4
 
+    # NOTE! uvicorn command line arg names preferable,
+    #   added on an as-needed basis!
     p.add_argument('--host', default=def_host, type=str,
                    help=f"addr to listen on [default: {def_host}]")
 
     p.add_argument('--port', default=def_port, type=int,
                    help=f"port to listen on [default: {def_port}]")
+
+    p.add_argument('--workers', default=def_workers, type=int,
+                   help=f"web server worker processes [default: {def_workers}]")
 
     # info logging before this call unlikely to be seen:
     args = p.my_parse_args()       # parse logging args, output start message
@@ -34,8 +40,9 @@ if __name__ == '__main__':
     # https://github.com/tiangolo/fastapi/issues/1508#issuecomment-723457712
     log_config = None
 
-    uvicorn.run(server.app,
+    uvicorn.run(app="server:app",
                 host=args.host,
                 log_config=log_config,
                 port=args.port,
-                timeout_keep_alive=500)
+                timeout_keep_alive=500,
+                workers=args.workers)
