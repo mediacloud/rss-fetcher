145dcb5f8308ddcde74ef38471b47f6e2895ec06
Phil Budne <pbudne@angwin.cs.umass.edu>
2024-08-15 23:16:41 -0400

    add stories_sources_id index

---
+++ fetcher/database/versions/20240815_2313_add_stories_sources_id.py
@@ -0,0 +1,24 @@
+"""add stories_sources_id
+
+Revision ID: 73d7a4ca0e7b
+Revises: 5332cd0c7e48
+Create Date: 2024-08-15 23:13:18.484530
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '73d7a4ca0e7b'
+down_revision = '5332cd0c7e48'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_index('stories_sources_id', 'stories', ['sources_id'])
+
+
+def downgrade():
+    op.drop_index('stories_sources_id', 'stories')
