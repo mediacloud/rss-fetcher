f073bc07f5f9757841844c7b4af3e5bfe4ffd322
Phil Budne <pbudne@angwin.cs.umass.edu>
2024-08-16 12:32:48 -0400

    server/util.py: typing improvements

--- server/util.py
+++ server/util.py
@@ -5,16 +5,30 @@ import inspect
 from itertools import chain
 import logging
 import time
-from typing import Any, Callable, Dict, List, Optional, TypedDict, Union
+from typing import Any, Callable, Coroutine, Literal, TYPE_CHECKING, TypedDict
+
+from fastapi.types import DecoratedCallable
 
 from fetcher import VERSION
 from fetcher.stats import Stats
 
+if TYPE_CHECKING:
+    from mypy_extensions import VarArg, KwArg
 
-class Status(Enum):
-    OK = 'ok'
-    ERROR = 'error'
+    # return type for api_method decorator
+    APIMethodRet =  Callable[[VarArg(Any), KwArg(Any)], Coroutine[Any, Any, "ApiResults"]]
+else:
+    APIMethodRet = Any
 
+
+# upper case due to having used Enum.name
+# changing would effect API users & stats labels
+OKStatus = Literal['OK']
+ErrorStatus = Literal['ERROR']
+Status = OKStatus | ErrorStatus
+
+STATUS_OK: OKStatus = 'OK'
+STATUS_ERROR: ErrorStatus = 'ERROR'
 
 class TimeSeriesDatum(TypedDict):
     date: dt.date
@@ -22,74 +36,51 @@ class TimeSeriesDatum(TypedDict):
     type: str
 
 
-TimeSeriesData = List[TimeSeriesDatum]
+TimeSeriesData = list[TimeSeriesDatum]
 
 logger = logging.getLogger(__name__)
 
 
 class ApiResultBase(TypedDict):
-    status: str
     duration: int               # ms
     version: str
 
 
-class ApiResultOK(ApiResultBase):  # when status == Status.OK.name
-    results: Dict
+class ApiResultOK(ApiResultBase):
+    status: OKStatus
+    results: dict
 
 
-class ApiResultERROR(ApiResultBase):  # when status == Status.ERROR.name
+class ApiResultERROR(ApiResultBase):
+    status: ErrorStatus
     statusCode: int
     message: str
 
 
-ApiResults = Union[ApiResultOK, ApiResultERROR]
-
-
-def _error_results(message: str, start_time: float, name: str,
-                   status_code: int = 400) -> ApiResultERROR:
-    """
-    Central handler for returning error messages.
-    :param message:
-    :param start_time:
-    :param status_code:
-    :return:
-    """
-    status = Status.ERROR
-    return {
-        'status': status.name,
-        'statusCode': status_code,
-        'duration': _duration(start_time, status, name),
-        'message': message,
-        'version': VERSION,
-    }
+ApiResults = ApiResultOK | ApiResultERROR
 
 
 def _duration(start_time: float, status: Status, name: str) -> int:
     """
-    return request duration in ms.
-    also report stats based on request name & status
+    return request duration in ms for ApiResultBase "duration".
+    also log and report stats based on request name & status
     """
+    # due to having used enum.name; used as a stats label!
     sec = (time.time() - start_time) if start_time else 0
     stats = Stats.get()
     stats.incr(
         'api.requests', labels=[
-            ('status', status.name), ('name', name)])
+            ('status', status), ('name', name)])
     stats.timing('duration', sec)  # could label, but more expensive
-    logger.info(
-        f"endpoint: {name}, status: {status.name}, duration: {sec} sec")
+    logger.info("endpoint: %s, status: %s, duration: %.6f sec",
+                name, status, sec)
     return int(round(sec * 1000))
 
 
-# Phil: only working type signature I've found requires mypy to be installed for normal execution:
-# from fastapi.types import DecoratedCallable
-# from mypy_extensions import VarArg, KwArg
-# def api_method(func: DecoratedCallable) -> Callable[[VarArg(Any),
-# KwArg(Any)], ApiResults]:
-def api_method(func: Any) -> Any:
+def api_method(func: DecoratedCallable) -> APIMethodRet:
     """
-    Helper to wrap API method responses and add metadata.
+    Decorator for API methods: wrap responses and add metadata (version, duration, etc)
     Use this in server.py and it will add stuff like the
-    version to the response.
 
     Plus it handles errors in one place, and supresses ones we don't care to log to Sentry.
     """
@@ -98,20 +89,33 @@ def api_method(func: Any) -> Any:
         start_time = time.time()
         # could use __qualname__ if needed:
         name = f"{func.__module__}.{func.__name__}"
+        status: Status
         try:
             if inspect.iscoroutinefunction(func):
                 results = await func(*args, **kwargs)
             else:
                 results = func(*args, **kwargs)
-            status = Status.OK
-            return {            # update ApiResultOK if adding items!
-                'version': VERSION,
-                'status': status.name,
+            status = STATUS_OK
+            return {            # ApiResultOK
                 'duration': _duration(start_time, status, name),
+                'version': VERSION,
+                'status': status,
                 'results': results,
             }
         except Exception as e:
             # log other, unexpected, exceptions to Sentry
             logger.exception(e)
-            return _error_results(str(e), start_time, name)
+
+            # NOTE! str(Exception("foo")) returns "foo"
+            message = str(e)
+
+            status = STATUS_ERROR
+            return {            # ApiResultError
+                'duration': _duration(start_time, status, name),
+                'version': VERSION,
+                'status': status,
+                'statusCode': 400,
+                'message': message,
+            }
+
     return wrapper
