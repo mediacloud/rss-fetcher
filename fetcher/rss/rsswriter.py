5eedd2e16d677fa5273696d231050f2695ba8723
Phil Budne <pbudne@angwin.cs.umass.edu>
2024-08-23 21:54:49 -0400

    gen_daily_story_rss.py 8x speedup
    * fetcher/rss/rsswriter.py: Create RssWriter class: read item.template and Templatify once
    * scripts/gen_daily_story_rss.py: use RssWriter, use range query for date rather than cast

--- fetcher/rss/rsswriter.py
+++ fetcher/rss/rsswriter.py
@@ -32,43 +32,47 @@ def _int(val: Optional[int]) -> str:
     return str(val)
 
 
-def add_header(file: Optional[TextIO], today: dt.date) -> str:
-    with open(os.path.join(template_path, "header.template")) as f:
-        template_str = f.read()
-    tm = Template(template_str)
-    content = tm.render(
-        day=today.strftime("%Y-%m-%d"),
-        now=formatdate(),
-        version=VERSION)
-    if file:
-        file.write(content)
-    return content
+class RssWriter:
+    def __init__(self, file: Optional[TextIO]):
+        self.file = file
+        with open(os.path.join(template_path, "item.template")) as f:
+            template_str = f.read()
+        self.item_template = Template(template_str)
 
+    def add_header(self, today: dt.date) -> str:
+        with open(os.path.join(template_path, "header.template")) as f:
+            template_str = f.read()
+            tm = Template(template_str)
+        content = tm.render(
+            day=today.strftime("%Y-%m-%d"),
+            now=formatdate(),
+            version=VERSION)
+        if self.file:
+            self.file.write(content)
+        return content
 
-def add_item(file: Optional[TextIO], link: str,
-             pub_date: dt.datetime, domain: str, title: Optional[str],
-             feed_url: Optional[str] = None,
-             feed_id: Optional[int] = None,
-             source_id: Optional[int] = None) -> str:
-    with open(os.path.join(template_path, "item.template")) as f:
-        template_str = f.read()
-    tm = Template(template_str)
-    date_for_output = ""
-    if pub_date is not None:
-        date_for_output = formatdate(pub_date.timestamp())
-    content = tm.render(link=_escape(link), pub_date=date_for_output, domain=_escape(domain),
-                        title=_escape(title), feed_url=_escape(feed_url),
-                        sources_id=_int(source_id), feed_id=_int(feed_id))
-    if file:
-        file.write(content)
-    return content
+    def add_item(self,
+                 link: str, pub_date: dt.datetime,
+                 domain: str, title: Optional[str],
+                 feed_url: Optional[str] = None,
+                 feed_id: Optional[int] = None,
+                 source_id: Optional[int] = None) -> str:
+        date_for_output = ""
+        if pub_date is not None:
+            date_for_output = formatdate(pub_date.timestamp())
+        content = self.item_template.render(
+            link=_escape(link), pub_date=date_for_output, domain=_escape(domain),
+            title=_escape(title), feed_url=_escape(feed_url),
+            sources_id=_int(source_id), feed_id=_int(feed_id))
+        if self.file:
+            self.file.write(content)
+        return content
 
-
-def add_footer(file: Optional[TextIO]) -> str:
-    with open(os.path.join(template_path, "footer.template")) as f:
-        template_str = f.read()
-    tm = Template(template_str)
-    content = tm.render()
-    if file:
-        file.write(content)
-    return content
+    def add_footer(self) -> str:
+        with open(os.path.join(template_path, "footer.template")) as f:
+            template_str = f.read()
+        tm = Template(template_str)
+        content = tm.render()
+        if self.file:
+            self.file.write(content)
+        return content
--- scripts/gen_daily_story_rss.py
+++ scripts/gen_daily_story_rss.py
@@ -9,7 +9,7 @@ from fetcher.config import conf
 from fetcher.database.engine import engine
 from fetcher.logargparse import LogArgumentParser
 import fetcher.path as path
-import fetcher.rss.rsswriter as rsswriter
+from fetcher.rss.rsswriter import RssWriter
 from fetcher.stats import Stats
 import fetcher.util as util
 
@@ -48,35 +48,50 @@ if __name__ == '__main__':
 
     # generate a file for each of the last N days (skipping today, which might
     # still be running)
+    query = f"""
+                select s.id, s.url, s.published_at, s.domain, s.title, s.feed_id, s.sources_id, f.url as feed_url
+                from stories s
+                left join feeds f
+                on s.feed_id = f.id
+                where s.fetched_at >= :day and
+                      s.fetched_at < :day_after and
+                      s.url is not NULL and s.feed_id = f.id
+                """
+    qtext = text(query)
+
+    day = today
+    day_str = day.strftime("%Y-%m-%d")
+    one_day = dt.timedelta(1)
+
     for d in range(1, args.days):
         try:
-            day = today - dt.timedelta(d)
+            day_after = day
+            day_after_str = day_str
+
+            day -= one_day
             day_str = day.strftime("%Y-%m-%d")
-            logger.info(f" Working on {day_str} (day {d})")
+
+            logger.info(
+                f" Working on {day_str} (day {d}) day_after {day_after_str}")
             # only do this day if it doesn't exist already
             filename = f"mc-{day_str}.rss.gz"
             compressed_filepath = os.path.join(target_dir, filename)
             if not os.path.exists(compressed_filepath):
                 tmp_path = compressed_filepath + '.tmp'
                 with gzip.open(tmp_path, 'wt') as outfile:
+                    rsswriter = RssWriter(outfile)
                     incr_files('created')
-                    rsswriter.add_header(outfile, day)
+                    rsswriter.add_header(day)
                     # grab the stories fetched on that day
                     # (ignore ones that didn't have URLs - ie. podcast feeds, which have `<enclosure url="...">` instead)
                     story_count = 0
-                    query = f"""
-                        select s.id, s.url, s.published_at, s.domain, s.title, s.feed_id, s.sources_id, f.url as feed_url
-                        from stories s
-                        left join feeds f
-                        on s.feed_id = f.id
-                        where s.fetched_at::date = '{day_str}'::date and s.url is not NULL and s.feed_id = f.id
-                    """
                     with engine.begin() as connection:  # will auto-close
-                        result = connection.execute(text(query))
+                        result = connection.execute(
+                            qtext, {"day": day_str, "day_after": day_after_str})
                         for row in result:
                             story = row._asdict()
                             try:
-                                rsswriter.add_item(outfile, story['url'], story['published_at'], story['domain'],
+                                rsswriter.add_item(story['url'], story['published_at'], story['domain'],
                                                    util.clean_str(
                                     story['title']) if 'title' in story else '',
                                     feed_url=story['feed_url'] or '',
@@ -91,7 +106,7 @@ if __name__ == '__main__':
                                     f"Skipped story {story['id']} - {e}")
                                 incr_stories('skipped')
                             story_count += 1
-                    rsswriter.add_footer(outfile)
+                    rsswriter.add_footer()
                 os.rename(tmp_path, compressed_filepath)
                 logger.info(f"   Found {story_count} stories")
                 logger.info(f"   Wrote out to {compressed_filepath}")
